eb99fe6434c05e8c3e5bd5433db99cf139c7c8c7
aeg224@g.harvard.edu <aeg224@g.harvard.edu>
2020-10-15 13:05:21 -0400

    created game mecahnics with computed property

--- project1/myScript.js
+++ project1/myScript.js
@@ -2,15 +2,37 @@ let app = new Vue({
 	el: '#gameP1',
 	data: {
 		selection: "",
-		gameHistory: []	
+		gameHistory: [],
+		options: ['rock', 'paper', 'scissors'],
+		computerSelection: '',
+		gameStarted: false,
+
 	},
         methods: {
 
-        runGame(){
-		console.log(this.selection);        
-		}
+			runGame(){
+				console.log(this.selection); 
+				this.computerSelection=this.options[this.getRandomInt()];
+				console.log(this.computerSelection);
+				this.gameStarted=true;
+				
+			},
+			getRandomInt(){
+				return Math.floor(Math.random() * this.options.length);
+			}
         },
 
-	getComputerSelection(){},
-	getResults(){}
+		computed: {
+			feedback(){
+				if (this.selection == this.computerSelection){
+					return 'Draw!';
+				}
+				else if (this.selection == 'rock' && this.computerSelection == 'paper' || this.selection == 'paper' && this.computerSelection == 'scissors' || this.selection == 'scissors' && this.computerSelection == 'rock'){
+					return 'You Lose';
+				}
+				else {
+					return 'You Win';
+				}
+			}
+		}
 })
