eb1239ed2b665fd8f8174fa22eb394ad47d85a9f
aeg224@g.harvard.edu <aeg224@g.harvard.edu>
2020-11-03 14:50:25 -0500

    Completed SPA Portion

---
+++ zipfoods/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="ZipFoods logo" id="logo" src="@/assets/images/zipfoods-logo.png">
+
+
+    <home-page></home-page>
+    <products-page></products-page>
+    <categories-page></categories-page>
+
+  </div>
+</template>
+
+<script>
+import HomePage from '@/components/pages/HomePage.vue';
+import ProductsPage from '@/components/pages/ProductsPage.vue';
+import CategoriesPage from '@/components/pages/CategoriesPage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    'home-page': HomePage,
+    'products-page': ProductsPage,
+    'categories-page': CategoriesPage,
+  }
+}
+</script>
+
+<style lang='scss'>
+@import '@/assets/scss/zipfoods.scss';
+</style>
+}
---
+++ zipfoods/src/components/pages/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <p> Your One Stop Shop For Groceries In The DC Area!</p>
+    <show-featured :category='"vegetables"'></show-featured>
+</div>
+</template>
+
+<script>
+import ShowFeatured from '@/components/ShowFeatured.vue';
+
+export default {
+    name: '',
+    components: {
+    'show-featured': ShowFeatured,
+  },
+
+
+};
+</script>
+
+<style>
+</style>
