6f003c1482dc96fbd1c1c830b5d121c1f00e923e
aeg224@g.harvard.edu <aeg224@g.harvard.edu>
2020-12-15 21:55:12 -0500

    base url in axios call;

--- p3/src/app.js
+++ p3/src/app.js
@@ -1,9 +1,8 @@
 export const axios = require('axios').create({
-    baseURL: 'http://e28-api.aeg224.loc/',
+    baseURL: process.env.VUE_APP_API_URL ?? 'http://e28-api.aeg224.loc/',
     responseType: 'json',
     withCredentials: true
 })
-//    baseURL: process.env.VUE_APP_API_URL ?? 'http://e28-api.aeg224.loc/',
 
 export const axios_nps_api = require('axios').create({
     baseURL: '',
